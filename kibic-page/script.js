df8a82cb092900c78d3bc1efaca3905c3336e990
Nick <nikeca17_kn@abv.bg>
2018-10-14 16:54:46 +0300

    updates

--- kibic-page/script.js
+++
@@ -1,8 +0,0 @@
-//automatically updates the years of the copyright
-document
-  .getElementById("footer")
-  .append(
-    "&copy;  Copyright " +
-      new Date().getFullYear() +
-      " AUBG OLYMPICS. All rights reserved."
-  );
